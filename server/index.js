b88c3736c735c8c0167928f6e2ca506488ae47da
NoumanLF <bcsf19a017@pucit.edu.pk>
2022-11-03 12:44:22 -0700

    Starting Server Side

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from "express";
-
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenve from "dotenv";
+import router from "./Routes/Users.js"
+dotenve.config()
 const app = express()
 
-app.listen(5000, () => {
-    console.log("Listening")
-})
+app.use(bodyParser.json({extended : true}))
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.use("/", router)
+
+const CONNECTION_URL = "mongodb+srv://Nouman:Laptop2017@eccomerce.s721mtr.mongodb.net/Eccomerce?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+mongoose.connect(CONNECTION_URL , {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log("Server is Running on PORT :" + PORT )))
+    .catch((error) => console.log(error.message));
