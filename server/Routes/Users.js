15521b9352e9a187b74171cb55937adab5ffb2f8
NoumanLF <bcsf19a017@pucit.edu.pk>
2022-11-19 11:56:15 -0800

    Adding user Routes

--- server/Routes/Users.js
+++ server/Routes/Users.js
@@ -1,8 +1,14 @@
 import express from "express"
+import { deleteUser, getAllUsers, getUser, login, signUp, updateUser } from "../Controllers/User"
+import {AuthUser, verifyTokenAndAdmin} from '../Middleware/AuthToken'
 const router = express.Router()
-import {login} from "../Controllers/User.js"
-router.get("/dummy", (req,res) => {
-    console.log("Request Success")
-})
-router.get("/login" , login)
+
+
+router.post('/register' , signUp)
+router.post('/login'  , login)
+router.put('/:id' , AuthUser, updateUser)
+router.delete('/:id' , AuthUser , deleteUser)
+router.get('/find/:id', verifyTokenAndAdmin , getUser)
+router.get('/' , verifyTokenAndAdmin , getAllUsers)
+
 export default router
